7ce28d25f6317efc67b561d09f54f7aea3b566e6
Haxxer <adamoresten@gmail.com>
2023-12-02 18:52:12 +0000

    Day 2

--- days/base_test.py
+++ days/base_test.py
@@ -27,21 +27,25 @@ class BaseUnitTest(unittest.TestCase):
         except FileNotFoundError:
             pass
 
-    def test_loaded(self):
-        self.assertNotEqual(self.sample, "", "Sample input is empty")
-        self.assertNotEqual(self.input, "", "Input is empty")
-
     def validate_sample(self, input):
-        self.assertEqual(input, self.expected_sample_result, "Sample result is not correct, expected {} but got {}!".format(self.expected_sample_result, input))
+        if not self.sample:
+            return
+        self.assertEqual(input, self.expected_sample_result, "Sample result is not correct, expected {} but got {}".format(self.expected_sample_result, input))
     
     def validate_input(self, input):
-        self.assertEqual(input, self.expected_input_result, "Input result is not correct, expected {} but got {}!".format(self.expected_input_result, input))
+        if not self.input:
+            return
+        self.assertEqual(input, self.expected_input_result, "Input result is not correct, expected {} but got {}".format(self.expected_input_result, input))
 
     def test_sample(self):
+        if not self.sample:
+            return
         sample_result = self.run_day(self.sample)
         self.validate_sample(sample_result)
 
     def test_input(self):
+        if not self.input:
+            return
         input_result = self.run_day(self.input)
         self.validate_input(input_result)
 
